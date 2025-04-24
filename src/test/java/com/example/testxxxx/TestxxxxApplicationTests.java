73805b30387e707748efbe4bac90f0dbe32689f8
rocky-peng <rocky.peng@qq.com>
2025-04-24 14:59:08 +0800

    init

---
+++ src/test/java/com/example/testxxxx/TestxxxxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testxxxx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestxxxxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
