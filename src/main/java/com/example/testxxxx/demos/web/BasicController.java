25110bdcfbe8faf6e26d1477db1037f5c7ae0b37
rocky-peng <rocky.peng@qq.com>
2025-04-24 17:22:18 +0800

    init

--- src/main/java/com/example/testxxxx/demos/web/BasicController.java
+++ src/main/java/com/example/testxxxx/demos/web/BasicController.java
@@ -32,6 +32,7 @@ public class BasicController {
     @RequestMapping("/hello")
     @ResponseBody
     public String hello(@RequestParam(name = "name", defaultValue = "unknown user") String name) {
+        System.out.println("hello " + name);
         return "Hello " + name;
     }
 
